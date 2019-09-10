1e403916e9f1ab7730467f88ff58e96b3ac8a75d
酒井典昭 <sakainoriaki@sakainoriakinoMacBook-puro.local>
2019-09-10 21:56:59 +0900

    00003

---
+++ cp9-3/cp9-3/SecondViewController.swift
@@ -0,0 +1,42 @@
+//
+//  SecondViewController.swift
+//  cp9-3
+//
+//  Created by 酒井典昭 on 2019/09/10.
+//  Copyright © 2019 典昭酒井. All rights reserved.
+//
+
+import UIKit
+import PGFramework
+
+
+// MARK: - Property
+class SecondViewController: BaseViewController {
+    
+}
+
+// MARK: - Life cycle
+extension SecondViewController {
+    override func loadView() {
+        super.loadView()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+}
+
+// MARK: - Protocol
+extension SecondViewController {
+    
+}
+
+// MARK: - method
+extension SecondViewController {
+    
+}
+
